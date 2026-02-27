4bb21275e2fb75b24be4b050d8e7f6732a8caa39
goyanipriya3-dot <goyanipriya3@gmail.com>
2026-02-27 13:05:58 +0000

    lecture 2

---
+++ C++ 2.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int amount 1;
+     cin>>amount1;
+
+     int amount 2;
+     cin>>amount2;
+
+     int sum= amount1 + amount2;
+     Cout<<sum\n
+     Cout<<sum<<end l;
+
+     return 0;
+}
